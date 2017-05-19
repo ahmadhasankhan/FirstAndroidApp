47c1e8c6f969f971eb5d0c8b3864d289ba6f567e
Ahmad Hassan <er.ahmad.hassan@gmail.com>
2017-05-20 02:41:19 +0530

    first commit

---
+++ app/src/main/java/com/hassan/ahmad/firstapp/MyWorker.java
@@ -0,0 +1,11 @@
+package com.hassan.ahmad.firstapp;
+
+/**
+ * Created by ahmadhassan on 5/20/17.
+ */
+
+public class MyWorker {
+    public static int doubleTheValue(int value){
+        return value*2;
+    }
+}
